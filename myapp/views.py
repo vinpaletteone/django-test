db8c655462bbdf462890e9c4075fe950b881c38a
“vinpaletteone”  <binsuzy95@gmail.com>
2022-03-16 10:31:11 +0900

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse('Hello');
+
+def create(request):
+    return HttpResponse('create');
+
+def read(request, id):
+    return HttpResponse('read'+id);

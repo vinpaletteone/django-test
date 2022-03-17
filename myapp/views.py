7b6dbcfa00a5c4ff19ea5a911981beb777b2beb6
“vinpaletteone”  <binsuzy95@gmail.com>
2022-03-17 17:16:16 +0900

    삭제 기능 구현

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
 # from django.http import HttpResponse
 import imp
+from importlib.metadata import requires
 from turtle import title
+from xxlimited import new
 from django.shortcuts import render, HttpResponse, redirect
 from django.views.decorators.csrf import csrf_exempt
 
@@ -11,8 +13,18 @@ topics = [
     {'id':3, 'title':'model', 'body':'Model is...'}
 ]
 
-def HTMLTemplate(articleTag) : 
+def HTMLTemplate(articleTag, id=None) : 
     global topics
+    contextUI = ''
+    if id!=None :
+        contextUI = f'''
+            <li>
+                <form action='/delete/' method='post'>
+                    <input type='hidden' name='id' value={id}>
+                    <input type='submit' value='delete'>
+                </form>
+            </li>
+        '''
     ol = '';
     for topic in topics : 
         ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
@@ -27,6 +39,7 @@ def HTMLTemplate(articleTag) :
             {articleTag}
             <ul>
                 <li><a href='/create/'>create</a></li>
+                {contextUI}
             </ul>
         </body>
         </html>
@@ -67,4 +80,17 @@ def read(request, id):
     for topic in topics : 
         if topic['id'] == int(id) : 
             article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
-    return HttpResponse(HTMLTemplate(article));
+    return HttpResponse(HTMLTemplate(article, id));
+
+@csrf_exempt
+def delete(request):
+    global topics
+    if request.method == 'POST' :
+        id = request.POST['id']
+        newTopics = []
+        for topic in topics :
+            if topic['id'] != int(id) : 
+                newTopics.append(topic)
+        topics = newTopics;
+        
+        return redirect('/')

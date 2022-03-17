6ff3d7a340b8cd55ae21a830b491e6b1073d19a5
“vinpaletteone”  <binsuzy95@gmail.com>
2022-03-17 16:04:30 +0900

    읽기 상세보기 기능 구현

--- myapp/views.py
+++ myapp/views.py
@@ -9,24 +9,38 @@ topics = [
     {'id':3, 'title':'model', 'body':'Model is...'}
 ]
 
-def index(request):
+def HTMLTemplate(articleTag) : 
     global topics
     ol = '';
     for topic in topics : 
         ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
-    return HttpResponse(f'''
+
+    return f'''
         <html>
         <body>
-            <h1>Django</h1>
+            <h1><a href='/'>Django</a></h1>
             <ol>
                 {ol}
             </ol>
+            {articleTag}
         </body>
         </html>
-    ''');
+    ''';
+
+def index(request):
+    article = '''
+        <h2>Welcome</h2>
+        Hello, Django
+    '''
+    return HttpResponse(HTMLTemplate(article));
 
 def create(request):
     return HttpResponse('create');
 
 def read(request, id):
-    return HttpResponse('read'+id);
+    global topics
+    article = ''
+    for topic in topics : 
+        if topic['id'] == int(id) : 
+            article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+    return HttpResponse(HTMLTemplate(article));
